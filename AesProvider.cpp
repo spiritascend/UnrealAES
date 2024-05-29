4762cde9a73e59ede4c1fae376119fa7699e91af
Joe <25163821+spiritascend@users.noreply.github.com>
2024-05-29 10:44:32 -0400

    Speeding up the process of getting the AES key from the start offset

--- AesProvider.cpp
+++ AesProvider.cpp
@@ -13,23 +13,21 @@ std::string AesProvider::VectorToHexString(const std::vector<uint8_t>& vec) {
 std::string AesProvider::GetAESKey(FILE* file, long long startoffset) {
     std::vector<std::uint8_t> buffer(58);
     std::vector<std::uint8_t> aesbuffer(AES_BYTESIZE);
-    fseek(file, startoffset, SEEK_SET);
-    size_t bytesRead = fread(buffer.data(), 1, buffer.size(), file);
 
-    if (bytesRead < buffer.size()) {
-        throw std::runtime_error("Failed to read enough data from the file.");
+    if (fseek(file, startoffset, SEEK_SET) != 0) {
+        throw std::runtime_error("failed to seek from the starting offset");
     }
 
-    int numberofbytesinserted = 0;
-    int offset = 0;
+    size_t bytesRead = fread(buffer.data(), 1, buffer.size(), file);
+    if (bytesRead < buffer.size()) {
+        throw std::runtime_error("failed to read. bytes read exceeds the buffer size");
+    }
 
-    while (numberofbytesinserted < 32) {
-        if (numberofbytesinserted == 8) {
-            offset += 4;
-        }
-        std::memcpy(aesbuffer.data() + numberofbytesinserted, buffer.data() + offset, 4);
-        numberofbytesinserted += 4;
-        offset += 7;
+    constexpr int src_offsets[] = { 0, 7, 14, 21, 25, 32, 39, 46 };
+    constexpr int dest_offsets[] = { 0, 4, 8, 12, 16, 20, 24, 28 };
+
+    for (int i = 0; i < 8; ++i) {
+        std::memcpy(aesbuffer.data() + dest_offsets[i], buffer.data() + src_offsets[i], 4);
     }
 
     return VectorToHexString(aesbuffer);
